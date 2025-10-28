0144b76f7ee25406ecba8b9b252081fe052cef9b
mrisoli <celorisoli@gmail.com>
2025-10-28 11:43:55 -0500

    update themes

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -1,27 +1,27 @@
 return {
-  "github/copilot.vim",
-  "editorconfig/editorconfig-vim",
-  "tpope/vim-fugitive",
+	"github/copilot.vim",
+	"editorconfig/editorconfig-vim",
+	"tpope/vim-fugitive",
 
-  { "gennaro-tedesco/nvim-peekup", lazy = true },
-  { "simrat39/symbols-outline.nvim", lazy = true },
-  { "nvim-tree/nvim-web-devicons", lazy = true },
+	{ "gennaro-tedesco/nvim-peekup", lazy = true },
+	{ "simrat39/symbols-outline.nvim", lazy = true },
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
 
-  "jremmen/vim-ripgrep",
+	"jremmen/vim-ripgrep",
 
-  {
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup()
-    end,
-  },
+	{
+		"kylechui/nvim-surround",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
 
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-  "nathangrigg/vim-beancount",
+	"nathangrigg/vim-beancount",
 }
